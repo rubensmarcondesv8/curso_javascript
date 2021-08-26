42e962a65197d1375e3c7667f17e06f49965da21
rubensmarcondesv8 <rubensmarcondesv8@gmail.com>
2021-08-26 18:16:20 -0300

    final aula 11

---
+++ aula11/exer001.js
@@ -0,0 +1,6 @@
+var vel = 56
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log("Você ultrapassou a velocida permitida- MULTADO")
+}
+console.log("Dirija sempre com cinto de segurança!!!")
